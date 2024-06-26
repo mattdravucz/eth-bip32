9a8b9cc077ad5148b7b433d1d7fe748763c6619f
mattd <matt.dravucz@gmail.com>
2024-06-26 09:56:03 +0100

    compressed refactored version

---
+++ compressed.py
@@ -0,0 +1,65 @@
+import hashlib
+import hmac
+import ecdsa
+from eth_utils import keccak
+
+BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+def sha256(data):
+    return hashlib.sha256(data).digest()
+
+def b58decode(s):
+    num = sum(BASE58_ALPHABET.index(c) * 58**i for i, c in enumerate(s[::-1]))
+    return num.to_bytes((num.bit_length() + 7) // 8, 'big')
+
+def b58decode_check(s):
+    decoded = b58decode(s)
+    if sha256(sha256(decoded[:-4]))[:4] != decoded[-4:]:
+        raise ValueError("Invalid checksum")
+    return decoded[:-4]
+
+def derive_child_public_key(parent_public_key, parent_chain_code, index):
+    data = parent_public_key + index.to_bytes(4, 'big')
+    i = hmac.new(parent_chain_code, data, hashlib.sha512).digest()
+    il, ir = i[:32], i[32:]
+    
+    curve = ecdsa.SECP256k1
+    point = ecdsa.VerifyingKey.from_string(parent_public_key, curve=curve).pubkey.point
+    child_point = point + curve.generator * int.from_bytes(il, 'big')
+    
+    child_public_key = ecdsa.VerifyingKey.from_public_point(child_point, curve=curve).to_string("compressed")
+    return child_public_key, ir
+
+def checksum_encode(address):
+    address = address.lower().replace("0x", "")
+    keccak_hash = keccak(bytes(address, encoding='utf-8')).hex()
+    return "0x" + ''.join(c.upper() if int(keccak_hash[i], 16) >= 8 else c for i, c in enumerate(address))
+
+class HDWallet:
+    def __init__(self, xpub):
+        data = b58decode_check(xpub)
+        self.chain_code = data[13:45]
+        self.public_key = data[45:]
+        self.path = None
+
+    def derive_path(self, path):
+        indices = [int(i) for i in path.split('/') if i.isnumeric()]
+        derived = self
+        derived.path = path.rstrip('/')
+        for index in indices:
+            derived.public_key, derived.chain_code = derive_child_public_key(derived.public_key, derived.chain_code, index)
+        return derived
+
+    def eth_address(self):
+        uncompressed = ecdsa.VerifyingKey.from_string(self.public_key, curve=ecdsa.SECP256k1).to_string("uncompressed")[1:]
+        address = keccak(uncompressed)[-20:].hex()
+        return checksum_encode(address)
+
+if __name__ == "__main__":
+    xpub = "xpub6CqGnXKKteadngNJV3YFVCawwJL2nzBkRj7VYZRSAsLpdmLZ4WnRKhqYZaXbqDtWqqAdyuQCMnV2ECgzRFMNiskHscRg51XN5iVzMvgRtdt"
+    path = "m/0/1/1/0"
+    
+    wallet = HDWallet(xpub)
+    derived_wallet = wallet.derive_path(path)
+    ethereum_address = derived_wallet.eth_address()
+    print(f"Derived Ethereum address: {ethereum_address}")
