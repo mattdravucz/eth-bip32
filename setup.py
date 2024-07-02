9b30c49740be6f555ea34f9e30851822dcc73e92
mattd <matt.dravucz@gmail.com>
2024-07-02 20:19:45 +0100

    add descriptions to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, Extension, find_packages
 
+
+with open("README.md", "rt") as fh:
+    long_description = fh.read()
+
 keccak256_module = Extension('eth_bip32.keccak._keccak256',
                              sources=['src/eth_bip32/keccak/keccak256.c', 'src/eth_bip32/keccak/keccak256_module.c'],
                              include_dirs=['src/eth_bip32/keccak'])
@@ -7,6 +11,9 @@ keccak256_module = Extension('eth_bip32.keccak._keccak256',
 setup(
     name="eth_bip32",
     version="0.1.0",
+    description="Deriving Ethereum addresses from HD wallets using extended public keys (xpub)",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     ext_modules=[keccak256_module],
