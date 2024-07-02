ecb2324b4ea8ec9374dbae1f7f5e13cfdcb231f1
mattd <matt.dravucz@gmail.com>
2024-07-02 15:29:43 +0100

    back to 0

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, Extension, find_packages
+import sys
+
+extra_compile_args = []
+extra_link_args = []
+
+if sys.platform == 'win32':
+    extra_compile_args.append('/DWIN32')
+    extra_link_args.append('/MANIFEST')
 
 keccak256_module = Extension('eth_bip32.keccak._keccak256',
                              sources=['src/eth_bip32/keccak/keccak256.c'],
-                             include_dirs=['src/eth_bip32/keccak'])
+                             include_dirs=['src/eth_bip32/keccak'],
+                             extra_compile_args=extra_compile_args,
+                             extra_link_args=extra_link_args)
 
 setup(
     name="eth_bip32",
